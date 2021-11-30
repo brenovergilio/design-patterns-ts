3228a96d17658429de249e2186ebd15047f006b2
Breno Vergilio <bjvergilio47@gmail.com>
2021-11-30 17:06:50 -0400

    strategy

---
+++ src/behavioural/strategy/index.ts
@@ -0,0 +1,9 @@
+import { DefaultDiscount } from './shopping-cart/default-discount';
+import { ECommerceShoppingCart } from './shopping-cart/ecommerce-shopping-cart';
+
+const shoppingCart = new ECommerceShoppingCart();
+shoppingCart.discount = new DefaultDiscount();
+shoppingCart.addProduct({ name: 'Produto 1', price: 50 });
+shoppingCart.addProduct({ name: 'Produto 2', price: 50 });
+console.log(shoppingCart.getTotal());
+console.log(shoppingCart.getTotalWithDiscount());
