a8e581062a832e4d0f4b690ecb5ab30a778012b4
Breno Vergilio <bjvergilio47@gmail.com>
2021-11-29 20:27:19 -0400

    composite

---
+++ src/structural/composite/product-composite.ts
@@ -0,0 +1,36 @@
+export abstract class ProductComponent {
+  abstract getPrice(): number;
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  add(product: ProductComponent): void {}
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  remove(product: ProductComponent): void {}
+}
+
+export class ProductLeaf extends ProductComponent {
+  constructor(public name: string, public price: number) {
+    super();
+  }
+
+  getPrice(): number {
+    return this.price;
+  }
+}
+
+export class ProductComposite extends ProductComponent {
+  private children: ProductComponent[] = [];
+
+  add(...products: ProductComponent[]): void {
+    products.forEach((product) => this.children.push(product));
+  }
+
+  remove(product: ProductComponent): void {
+    const productIndex = this.children.indexOf(product);
+    if (productIndex !== -1) this.children.splice(productIndex, 1);
+  }
+
+  getPrice(): number {
+    return this.children.reduce((sum, child) => sum + child.getPrice(), 0);
+  }
+}
